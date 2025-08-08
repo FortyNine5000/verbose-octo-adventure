ee5add05d026a7682ba85b185240f6b34d218119
FortyNine5000 <cycloid_slabs8k@icloud.com>
2025-08-08 12:19:22 -0700

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function RecipesPage() {
-  return (
-    <main className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">Recipe Library</h1>
-      <p>Client-side filterable recipe grid coming soon.</p>
-    </main>
-  );
-}
